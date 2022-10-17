0e7a4c86fc5a0e7742b238afda1b861edba52672
muharrem gem <muharremgem28@gmail.com>
2022-10-17 22:44:16 +0300

    user

--- rc08_lifecycle-useEffect/src/App.js
+++ rc08_lifecycle-useEffect/src/App.js
@@ -1,20 +1,22 @@
 import { useState } from "react";
 import LifeCycleMethods from "./components/LifeCycleMethods";
 import UseEffectHook from "./components/UseEffectHook";
+import User from "./components/User";
 
 function App() {
   const [show, setShow] = useState(true);
 
   return (
     <div className="container text-center mt-4">
-      <button className="btn btn-dark" onClick={() => setShow(!show)}>
+      {/* <button className="btn btn-dark" onClick={() => setShow(!show)}>
         {show ? "Hide" : "Show"}
-      </button>
+      </button> */}
 
       {/* Short Circuit */}
       {/* {show && <LifeCycleMethods />} */}
 
-      {show && <UseEffectHook />}
+      {/* {show && <UseEffectHook />} */}
+      <User />
     </div>
   );
 }
---
+++ rc08_lifecycle-useEffect/src/components/User.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const User = () => {
+  return (
+    <div>
+        <h1>Name</h1>
+        <img src="#" alt="" />
+        <p>Email</p>
+        <h5>DOB</h5>
+        
+
+    </div>
+  )
+}
+
+export default User
