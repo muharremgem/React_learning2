7f136d9eb39d03aa0e035b368f875651871d6c84
muharrem gem <muharremgem28@gmail.com>
2022-10-17 20:34:50 +0300

    commit

--- rc08_lifecycle-useEffect/src/components/LifeCycleMethods.jsx
+++ rc08_lifecycle-useEffect/src/components/LifeCycleMethods.jsx
@@ -8,7 +8,7 @@
 //* Ornegin bir component olsuturuldugunda, DOM'a basilsiginda,
 //* guncellendiginde veya DOM'dan kaldirildiginda bir seyler yapmak icin
 //* lifecycle metotlari kullanilabilir.
-//* En bilindik lifecycle metodu render() metodudur
+//* En bilindik lifecycle metodu render() metodudur.
 
 //* Bir component'in olsuturulmasi (constructor,
 //* Bir componentin DOM agacina eklenmesinin sonrasi(componentDidMount)
