f396f539e3b1c7fd3a770188f5393108770c3401
muharrem gem <muharremgem28@gmail.com>
2022-10-17 22:55:25 +0300

    commit

--- rc08_lifecycle-useEffect/src/components/User.jsx
+++ rc08_lifecycle-useEffect/src/components/User.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 const User = () => {
+  const [user, setUser] = useState("");
+
+  fetch("https://randomuser.me/api/")
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+
   return (
     <div>
-        <h1>Name</h1>
-        <img src="#" alt="" />
-        <p>Email</p>
-        <h5>DOB</h5>
-        
-
+      <h1>Name</h1>
+      <img src="#" alt="" />
+      <p>Email</p>
+      <h5>DOB</h5>
+      <button className="btn btn-danger">Get User</button>
     </div>
-  )
-}
+  );
+};
 
-export default User
+export default User;
