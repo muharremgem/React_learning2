ae5686935b93134b6ecb53b6193224b0d3bc6e29
muharrem gem <muharremgem28@gmail.com>
2022-10-24 21:55:18 +0300

    loading state

--- rc11-12_router-example/rc11-12_router-example-inClass/src/pages/PersonDetail.jsx
+++ rc11-12_router-example/rc11-12_router-example-inClass/src/pages/PersonDetail.jsx
@@ -16,18 +16,22 @@ const PersonDetail = () => {
 
   const [person, setPerson] = useState("");
   const [error, setError] = useState(false);
-  //const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch(`https://reqres.in/api/users/${id}`)
       .then((res) => {
         if (!res.ok) {
           setError(true);
+          setLoading(false);
           throw new Error("Something went wrong");
         }
         return res.json();
       })
-      .then((data) => setPerson(data.data))
+      .then((data) => {
+        setLoading(false);
+        setPerson(data.data);
+      })
       .catch((err) => console.log(err));
   }, []);
 
@@ -35,13 +39,15 @@ const PersonDetail = () => {
 
   if (error) {
     return <NotFound />;
-  } else if (!person) {
+  }
+  if (loading) {
     return (
       <div className="text-center">
         <h3>Data Loading...</h3>
       </div>
     );
-  } else {
+  }
+  if (!error && !loading) {
     return (
       <div className="container text-center">
         <h3>
